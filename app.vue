1babc95761e3ec48aa4208ed4fbf6de488686b4d
MOONcitizenX <mooncitizenx@gmail.com>
2023-09-17 16:27:42 +0300

    fix: articles swiper overflow on tablet/mobile

--- app.vue
+++ app.vue
@@ -6,96 +6,108 @@
         <NuxtPage />
       </main>
     </div>
-    <TheFooter/>
+    <TheFooter />
   </div>
 </template>
 
 <style lang="scss">
-  @import './assets/styles/style.scss';
-  @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600&family=IBM+Plex+Sans:ital,wght@1,700&family=Manrope:wght@200;300;400;500;600;700;800&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600&family=IBM+Plex+Sans:ital,wght@1,700&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Manrope:wght@200;300;400;500;600;700;800&display=swap');
+@import "./assets/styles/style.scss";
+@import url("https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600&family=IBM+Plex+Sans:ital,wght@1,700&family=Manrope:wght@200;300;400;500;600;700;800&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600&family=IBM+Plex+Sans:ital,wght@1,700&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Manrope:wght@200;300;400;500;600;700;800&display=swap");
 
-  @font-face {
-  font-family: 'Neue Machin';
-  src: url('./assets/fonts/NeueMachina-Regular.ttf') format('truetype');
+@font-face {
+  font-family: "Neue Machin";
+  src: url("./assets/fonts/NeueMachina-Regular.ttf") format("truetype");
   font-weight: 400;
   font-style: normal;
 }
 
-  @font-face {
-    font-family: 'Neue Machin';
-    src: url('./assets/fonts/NeueMachina-Medium.ttf') format('truetype');
-    font-weight: 500;
-    font-style: normal;
-  }
+@font-face {
+  font-family: "Neue Machin";
+  src: url("./assets/fonts/NeueMachina-Medium.ttf") format("truetype");
+  font-weight: 500;
+  font-style: normal;
+}
 
-  @font-face {
-    font-family: 'Neue Machin';
-    src: url('./assets/fonts/NeueMachina-Bold.ttf') format('truetype');
-    font-weight: 700;
-    font-style: normal;
-  }
+@font-face {
+  font-family: "Neue Machin";
+  src: url("./assets/fonts/NeueMachina-Bold.ttf") format("truetype");
+  font-weight: 700;
+  font-style: normal;
+}
 
-  * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-    }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-    body {
-      font-family: 'Neue Machin';
-    }
+body {
+  font-family: "Neue Machin";
+}
 
-    a, a:link, a:visited  {
-        text-decoration: none;
-    }
+a,
+a:link,
+a:visited {
+  text-decoration: none;
+}
 
-    a:hover  {
-        text-decoration: none;
-    }
+a:hover {
+  text-decoration: none;
+}
 
-    ul, ul li {
-    list-style: none;
-  }
+ul,
+ul li {
+  list-style: none;
+}
 
-    h1, h2, h3, p {
-      margin: 0;
-    }
+h1,
+h2,
+h3,
+p {
+  margin: 0;
+}
 
-  img {
-    vertical-align: top;
-  }
+img {
+  vertical-align: top;
+}
 
-  img, svg {
-    max-width: 100%;
-    height: auto;
-  }
+img,
+svg {
+  max-width: 100%;
+  height: auto;
+}
 
-  .wrap {
-    width: 100%;
-    min-height: 100vh;
-    height: 100vh;
-    max-height: max-content;
-  }
+.wrap {
+  width: 100%;
+  min-height: 100vh;
+  height: 100vh;
+  max-height: max-content;
+}
 
+main {
+  padding: 0 20px;
+}
+
+.main {
+  margin: 0 auto;
+}
+
+@media (max-width: 1400px) {
   main {
-    padding: 0 20px;
+    padding: 0 31px;
   }
+}
 
-  .main {
-    margin: 0 auto;
+@media (max-width: 744px) {
+  main {
+    overflow-x: hidden;
   }
+}
 
-  @media (max-width: 1400px) {
-      main {
-        padding: 0 31px;
-      }
-    }
-
-    
-  @media (max-width: 650px) {
-      main {
-        padding: 0 16px;
-      }
-    }
+@media (max-width: 650px) {
+  main {
+    padding: 0 16px;
+  }
+}
 </style>
