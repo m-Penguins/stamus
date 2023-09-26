1a1b970a32957ca6f2e4f6a4b291e96b56d22c21
RuslanRagilo <ruslanragilo7@gmail.com>
2023-09-26 10:50:41 +0300

    rename import yandex map

--- components/blocks/Map.vue
+++ components/blocks/Map.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tabs">
     <div class="tabs-title" v-html="text"></div>
-    <div  class="tabs2">
+    <div class="tabs2">
       <div class="tabs-wrapper">
         <div class="tabs-container">
           <button
@@ -17,19 +17,26 @@
       </div>
     </div>
     <div class="tab-content">
-      <div v-for="(tab, index) in tabs" :key="index" v-show="activeTab === index">
+      <div
+        v-for="(tab, index) in tabs"
+        :key="index"
+        v-show="activeTab === index"
+      >
         <div v-if="activeTab === index">
           <div class="tab-map">
             <div class="map-container">
               <client-only>
                 <YandexMap
-                v-bind="config"
+                  v-bind="config"
+                  :coordinates="currentCoordinates"
+                  :detailed-controls="detailedControls"
+                  :controls="controls"
+                  map-type="map"
+                >
+                  <YandexMarker
                     :coordinates="currentCoordinates"
-                    :detailed-controls="detailedControls"
-                    :controls="controls"
-                    map-type="map"
-                  >     
-                    <YandexMarker :coordinates="currentCoordinates" :marker-id="123">
+                    :marker-id="123"
+                  >
                   </YandexMarker>
                   <!-- <YmapMarker
               v-for="marker in tabs"
@@ -46,42 +53,45 @@
                 </YandexMap>
               </client-only>
             </div>
-            <elements-map-nav :info="tab"/>
+            <elements-map-nav :info="tab" />
           </div>
           <!-- <img class="tab-map" :src="assetsStore.useAsset(`images/${tab.image}`)" alt=""> -->
         </div>
       </div>
     </div>
-  </div> 
+  </div>
 </template>
 
 <script>
-import { useAssets } from '../../stores/useAsset';
-import { yandexMap, ymapMarker  } from 'vue-yandex-maps'
-import icon from '../../assets/images/icons/Telegram-fill.svg'
+import { useAssets } from "../../stores/useAsset";
+import {
+  yandexMap,
+  ymapMarker,
+} from "vue-yandex-maps/dist/vue-yandex-maps.esm";
+import icon from "../../assets/images/icons/Telegram-fill.svg";
 const DEFAULT_CENTER = [55.75222, 37.61556]; //mskcenter
 export default {
   components: { yandexMap, ymapMarker },
   props: {
-      dataInfo: {
-        type: Array,
-      },
-      text: {
-        type: String,
-        default: ''
-      },
-      config: {
+    dataInfo: {
+      type: Array,
+    },
+    text: {
+      type: String,
+      default: "",
+    },
+    config: {
       type: Object,
       default: () => ({
         settings: {
-          apiKey: 'b4ed0b4f-9710-4113-bfd7-ef2be1938fa2',
-          lang: 'ru_RU',
-          coordorder: 'latlong',
-          version: '2.1'
-        }
+          apiKey: "b4ed0b4f-9710-4113-bfd7-ef2be1938fa2",
+          lang: "ru_RU",
+          coordorder: "latlong",
+          version: "2.1",
+        },
       }),
-    }
     },
+  },
   data() {
     return {
       icon,
@@ -89,68 +99,68 @@ export default {
       scrollOffset: 0,
       tabs: [
         {
-          name: 'ул. Московская 140',
-          image: 'map.png',
-          hours: '8:00 до 21:00', 
-          tel: '+7 (999) 888 - 77 - 66', 
-          email:"hakurate@stamus-info.ru", 
-          address:"Краснодар, ул. Московская 140",
-          coordinates: [45.085805, 39.002218] 
+          name: "ул. Московская 140",
+          image: "map.png",
+          hours: "8:00 до 21:00",
+          tel: "+7 (999) 888 - 77 - 66",
+          email: "hakurate@stamus-info.ru",
+          address: "Краснодар, ул. Московская 140",
+          coordinates: [45.085805, 39.002218],
         },
         {
-          name: 'ул. Хакурате 34',
-          image: 'map.png',
-          hours: '8:00 до 21:00', 
-          tel: '+7 (999) 888 - 77 - 66', 
-          email:"hakurate@stamus-info.ru", 
-          address:"Краснодар, ул. Хакурате 34",
-          coordinates: [45.041031, 38.982473]
+          name: "ул. Хакурате 34",
+          image: "map.png",
+          hours: "8:00 до 21:00",
+          tel: "+7 (999) 888 - 77 - 66",
+          email: "hakurate@stamus-info.ru",
+          address: "Краснодар, ул. Хакурате 34",
+          coordinates: [45.041031, 38.982473],
         },
         {
-          name: 'ул. Мачуги 1/1',
-          image: 'map.png',
-          hours: '8:00 до 21:00', 
-          tel: '+7 (999) 888 - 77 - 66', 
-          email:"hakurate@stamus-info.ru", 
-          address:"Краснодар, ул. Мачуги 1/1",
-          coordinates: [45.014403, 39.063016]
+          name: "ул. Мачуги 1/1",
+          image: "map.png",
+          hours: "8:00 до 21:00",
+          tel: "+7 (999) 888 - 77 - 66",
+          email: "hakurate@stamus-info.ru",
+          address: "Краснодар, ул. Мачуги 1/1",
+          coordinates: [45.014403, 39.063016],
         },
         // {
         //   name: 'ул. Черкасская 17',
         //   image: 'map.png',
-        //   hours: '8:00 до 21:00', 
-        //   tel: '+7 (999) 888 - 77 - 66', 
-        //   email:"hakurate@stamus-info.ru", 
+        //   hours: '8:00 до 21:00',
+        //   tel: '+7 (999) 888 - 77 - 66',
+        //   email:"hakurate@stamus-info.ru",
         //   address:"Краснодар, ул. Черкасская 17"
         // },
         {
-          name: 'ул. Гимназическая 85',
-          image: 'map.png',
-          hours: '8:00 до 21:00', 
-          tel: '+7 (999) 888 - 77 - 66', 
-          email:"hakurate@stamus-info.ru", 
-          address:"Краснодар, ул. Гимназическая 85",
-          coordinates: [45.024673, 38.975978]
+          name: "ул. Гимназическая 85",
+          image: "map.png",
+          hours: "8:00 до 21:00",
+          tel: "+7 (999) 888 - 77 - 66",
+          email: "hakurate@stamus-info.ru",
+          address: "Краснодар, ул. Гимназическая 85",
+          coordinates: [45.024673, 38.975978],
         },
         {
-          name: 'Платановый бульвар 19/3',
-          image: 'map.png',
-          hours: '8:00 до 21:00', 
-          tel: '+7 (999) 888 - 77 - 66', 
-          email:"hakurate@stamus-info.ru", 
-          address:"Краснодар, Платановый бульвар 19/3",
-          coordinates: [45.033521, 38.909799]
+          name: "Платановый бульвар 19/3",
+          image: "map.png",
+          hours: "8:00 до 21:00",
+          tel: "+7 (999) 888 - 77 - 66",
+          email: "hakurate@stamus-info.ru",
+          address: "Краснодар, Платановый бульвар 19/3",
+          coordinates: [45.033521, 38.909799],
         },
         // {
         //   name: 'ул. Средняя 1/3',
         //   image: 'map.png',
-        //   hours: '8:00 до 21:00', 
-        //   tel: '+7 (999) 888 - 77 - 66', 
-        //   email:"hakurate@stamus-info.ru", 
+        //   hours: '8:00 до 21:00',
+        //   tel: '+7 (999) 888 - 77 - 66',
+        //   email:"hakurate@stamus-info.ru",
         //   address:"Краснодар, ул. Средняя 1/3"
         //   }
-        ]
-    }
+      ],
+    };
   },
   computed: {
     currentCoordinates() {
@@ -166,26 +176,28 @@ export default {
       const tabsWrapperWidth = this.$refs.tabsWrapper.offsetWidth;
       const numTabs = this.tabs.length;
       return tabsWrapperWidth / numTabs;
-    }
+    },
   },
-    setup() {
+  setup() {
     const assetsStore = useAssets();
     const coordinates = [45.085805, 39.002218];
-    const controls = ['fullscreenControl'];
-    const detailedControls = { zoomControl: { position: { right: 10, top: 50 } } };
+    const controls = ["fullscreenControl"];
+    const detailedControls = {
+      zoomControl: { position: { right: 10, top: 50 } },
+    };
     return {
       assetsStore,
       coordinates,
       controls,
       detailedControls,
       yandexMap,
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import '/assets/styles/style.scss';
+@import "/assets/styles/style.scss";
 .map-container {
   background-color: #f0f0f0;
   width: 100%;
@@ -197,9 +209,9 @@ export default {
   border-radius: 15px;
 }
 
-    .ymaps-2-1-79-inner-panes {
-      border-radius: 15px;
-    }
+.ymaps-2-1-79-inner-panes {
+  border-radius: 15px;
+}
 // .tabs2 {
 //   width: 100%;
 //   overflow-x: scroll;
@@ -234,7 +246,7 @@ export default {
 }
 
 .tabs-container::-webkit-scrollbar {
-    display: none;
+  display: none;
 }
 
 .tabs-container {
@@ -271,14 +283,14 @@ export default {
 
 button:focus::after,
 button.active::after {
-  content: '';
+  content: "";
   position: absolute;
   bottom: 0;
   left: 0;
   width: 100%;
   height: 1px;
   background-color: $hover;
-  transition: all .3s ease;
+  transition: all 0.3s ease;
 }
 
 @media (max-width: 1000px) {
@@ -302,12 +314,12 @@ button.active::after {
   }
 }
 
-  @media (max-width: 700px) {
-    .tabs-btn-base {
-      padding: 20px 0 80px 0;
-    }
-    .tab-map {
-      margin-bottom: 80px;
-    }
+@media (max-width: 700px) {
+  .tabs-btn-base {
+    padding: 20px 0 80px 0;
+  }
+  .tab-map {
+    margin-bottom: 80px;
+  }
 }
 </style>
