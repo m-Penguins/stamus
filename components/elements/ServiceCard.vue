c1b6c8daf396e9120c1ec4f32c2435bd49c678ce
MOONcitizenX <mooncitizenx@gmail.com>
2023-10-02 01:07:19 +0300

    feat: add docs slider

--- components/elements/ServiceCard.vue
+++ components/elements/ServiceCard.vue
@@ -1,21 +1,39 @@
 <template>
   <div>
-    <nuxt-link to="#" class="link">
+    <nuxt-link :to="service.link" class="link">
       <div class="service-card">
         <div class="service-card-container">
           <div class="grey-point-container">
-            <div class="service-card-category grey-point-container" v-for="(item, index) in service.tags" :key="item">
-              <p class="grey-point-text">{{ item }}
-              </p>
-              <div class="grey-point" v-if="index < service.tags.length - 1"></div>
+            <div
+              class="service-card-category grey-point-container"
+              v-for="(item, index) in service.tags"
+              :key="item"
+            >
+              <p class="grey-point-text">{{ item }}</p>
+              <div
+                class="grey-point"
+                v-if="index < service.tags.length - 1"
+              ></div>
             </div>
-        </div>
+          </div>
           <div class="service-card-title">{{ service.heading }}</div>
         </div>
         <div class="service-card-box">
-          <div v-if="service.isRecommended" class="service-card-box__type-isRecommended">Рекомендуем</div>
-          <div v-if="service.isPopular" class="service-card-box__type-isPopular">Популярное</div>
-          <div v-if="service.isDemand"  class="service-card-box__type-isDemand">Высокий спрос</div>
+          <div
+            v-if="service.isRecommended"
+            class="service-card-box__type-isRecommended"
+          >
+            Рекомендуем
+          </div>
+          <div
+            v-if="service.isPopular"
+            class="service-card-box__type-isPopular"
+          >
+            Популярное
+          </div>
+          <div v-if="service.isDemand" class="service-card-box__type-isDemand">
+            Высокий спрос
+          </div>
           <div class="service-card-box__price">от {{ service.price }} ₽</div>
         </div>
       </div>
@@ -24,18 +42,17 @@
 </template>
 
 <script>
-  export default {
-    props: {
-      service: {
-        type: Object
-      },
+export default {
+  props: {
+    service: {
+      type: Object,
     },
-  }
+  },
+};
 </script>
 
-
 <style lang="scss" scoped>
-@import '/assets/styles/style.scss';
+@import "/assets/styles/style.scss";
 
 .service-card {
   display: flex;
@@ -48,84 +65,83 @@
   border-radius: 10px;
   border: 1px solid $border-card;
 
-    &:hover {
-      border: 1px solid $dissabled;
+  &:hover {
+    border: 1px solid $dissabled;
+  }
+
+  .service-card-container {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .service-card-box {
+    display: flex;
+    gap: 10px;
+
+    &__price {
+      @include body-18-regular;
+      color: $hover;
     }
 
-    .service-card-container {
+    &__type-isRecommended {
       display: flex;
-      flex-direction: column;
-      gap: 10px;
+      align-items: center;
+      padding: 6px 10px 8px 10px;
+      border-radius: 5px;
+      background: rgba(23, 201, 137, 0.05);
+      @include body-12-regular;
+      color: $green;
+      width: max-content;
+    }
+
+    &__type-isPopular {
+      display: flex;
+      align-items: center;
+      padding: 6px 10px 8px 10px;
+      border-radius: 5px;
+      background: rgba(220, 101, 15, 0.05);
+      @include body-12-regular;
+      color: #dc650f;
+      width: max-content;
+    }
+
+    &__type-isDemand {
+      display: flex;
+      align-items: center;
+      padding: 6px 10px 8px 10px;
+      border-radius: 5px;
+      background: rgba(244, 81, 81, 0.05);
+      @include body-12-regular;
+      color: #f45151;
+      width: max-content;
+    }
+  }
+
+  .service-card-title {
+    @include body-16-regular;
+    color: $dark-blue-subtitle;
+  }
+}
+
+@media (max-width: 800px) {
+  .service-card {
+    display: block;
+
+    .service-card-container {
+      gap: 20px;
+      margin-bottom: 10px;
     }
 
     .service-card-box {
       display: flex;
-      gap: 10px;
+      flex-direction: column;
+      gap: 8px;
 
-      &__price {
-        @include body-18-regular;
-        color: $hover;
+      &__type {
+        margin-bottom: 10px;
       }
-
-      &__type-isRecommended {
-        display: flex;
-        align-items: center;
-        padding: 6px 10px 8px 10px;
-        border-radius: 5px;
-        background: rgba(23, 201, 137, 0.05);
-        @include body-12-regular;
-        color: $green;
-        width: max-content;
-      }
-
-      &__type-isPopular {
-        display: flex;
-        align-items: center;
-        padding: 6px 10px 8px 10px;
-        border-radius: 5px;
-        background: rgba(220, 101, 15, 0.05);
-        @include body-12-regular;
-        color: #DC650F;
-        width: max-content;
-      }
-
-      &__type-isDemand {
-        display: flex;
-        align-items: center;
-        padding: 6px 10px 8px 10px;
-        border-radius: 5px;
-        background: rgba(244, 81, 81, 0.05);
-        @include body-12-regular;
-        color: #F45151;
-        width: max-content;
-      }
-    }
-
-    .service-card-title {
-      @include body-16-regular;
-      color: $dark-blue-subtitle;
-    }
-}
-
-  @media (max-width: 800px) {
-    .service-card {
-      display: block;
-
-        .service-card-container {
-          gap: 20px;
-          margin-bottom: 10px;
-        }
-
-        .service-card-box { 
-          display: flex;
-          flex-direction: column;
-          gap: 8px;
-
-            &__type {
-              margin-bottom: 10px;
-            }
-        }
     }
   }
-
+}
 </style>
