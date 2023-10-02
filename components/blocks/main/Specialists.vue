a637b0e60e15f7b6956e702caf4ee673076c578c
MOONcitizenX <mooncitizenx@gmail.com>
2023-10-03 01:54:20 +0300

    feat: add main page 4specialists

--- components/blocks/main/Specialists.vue
+++ components/blocks/main/Specialists.vue
@@ -1,56 +1,95 @@
+<script setup>
+const props = defineProps(["mainSpecialists"]);
+
+const selectedIndex = ref(0);
+
+console.log(selectedIndex.value);
+</script>
+
 <template>
   <div class="container-size specialists">
     <div class="specialists-subject">
       <h2 class="specialists-heading-title">Наши специалисты</h2>
-      <elements-link-with-arrow type="true" title="Смотреть всех специалистов" href="/specialists"/>
+      <elements-link-with-arrow
+        type="true"
+        title="Смотреть всех специалистов"
+        href="/specialists"
+      />
     </div>
     <div class="specialists-container">
       <div class="specialists-list">
-        <button 
-          v-for="button in buttons" 
-          :key="button.id" 
-          :class="{ 'active': selectedButton === button.id }" 
-          @click="selectButton(button.id)"
-          class="specialists-btn">
-          <div class="div-with-line" v-if="selectedButton === button.id"></div>
+        <button
+          v-for="(person, index) in props.mainSpecialists"
+          :key="person.id"
+          :class="{ active: selectedIndex === index }"
+          @click="selectedIndex = index"
+          class="specialists-btn"
+        >
+          <div class="div-with-line" v-if="selectedIndex === index"></div>
           <div class="specialists-btn__img">
-            <img :src="assetsStore.useAsset(`${button.img}`)"/>
+            <img :src="person.img" />
           </div>
           <div class="specialists-box">
-            <div class="specialists-box__name">{{ button.name }}</div>
-            <div class="specialists-box__spec">{{ button.category }}</div>
+            <div class="specialists-box__name">{{ person.name }}</div>
+            <div class="specialists-box__spec">{{ person.category }}</div>
           </div>
         </button>
       </div>
       <div class="specialists-inner">
         <div class="specialists-inner__img">
-          <img :src="assetsStore.useAsset(`${selectedBigImg}`)"/>
+          <img :src="props.mainSpecialists[selectedIndex].imgBig" />
         </div>
         <div class="specialists-inner__info">
           <div class="specialists-inner__info-name">
-            <h2 class="specialists-heading">{{selectedName}}</h2>
-            <p class="specialists-desc">{{selectedSpeciality}}</p>
+            <h2 class="specialists-heading">
+              {{ props.mainSpecialists[selectedIndex].name }}
+            </h2>
+            <p class="specialists-desc">
+              {{ props.mainSpecialists[selectedIndex].category }}
+            </p>
           </div>
-          <!-- <div v-for="button in buttons" :key="button.id" > -->
-            <!-- <elements-specialists-info-card :info="buttons"/> -->
-          <!-- </div> -->
+
           <div class="specialists-inner__info-numbers">
             <div>
-              <p class="specialists-numbers">{{selectedExperience}}</p>
-              <p class="specialists-box__spec">лет опыта</p>
+              <p class="specialists-numbers">
+                {{ props.mainSpecialists[selectedIndex].experience.year }}
+              </p>
+              <p class="specialists-box__spec">
+                {{ props.mainSpecialists[selectedIndex].experience.text }}
+              </p>
             </div>
             <div>
-              <p class="specialists-numbers">{{selectedReview}}</p>
-              <p class="specialists-box__spec">отзыва</p>
+              <p class="specialists-numbers">
+                {{ props.mainSpecialists[selectedIndex].review.year }}
+              </p>
+              <p class="specialists-box__spec">
+                {{ props.mainSpecialists[selectedIndex].review.text }}
+              </p>
             </div>
             <div>
-              <p class="specialists-numbers">{{selectedConsultation}}</p>
-              <p class="specialists-box__spec">консультаций</p>
+              <p class="specialists-numbers">
+                {{ props.mainSpecialists[selectedIndex].consultation.year }}
+              </p>
+              <p class="specialists-box__spec">
+                {{ props.mainSpecialists[selectedIndex].consultation.text }}
+              </p>
             </div>
           </div>
           <div class="specialists-inner__info-btn">
-            <elements-button-base title="О докторе" @click="$router.push('/specialists/1')" class="specialists-btn-base"/>
-            <elements-link-with-arrow type="true" title="Смотреть портфолио" :to="'/specialists/1' +  '#portfolio'"/>
+            <elements-button-base
+              title="О докторе"
+              @click="
+                $router.push(
+                  `/specialists/${props.mainSpecialists[selectedIndex].id}`,
+                )
+              "
+              class="specialists-btn-base"
+            />
+            <elements-link-with-arrow
+              type="true"
+              title="Смотреть портфолио"
+              :to="`/specialists/${props.mainSpecialists[selectedIndex].id}#portfolio`"
+            />
           </div>
         </div>
       </div>
@@ -58,371 +97,287 @@
   </div>
 </template>
 
-<script>
-import { useAssets } from '../../../stores/useAsset';
-export default {
-  data() {
-    return {
-      selectedButton: 2,
-      selectedName: 'Унаньян Карина',
-      selectedImg: 'images/specialists/avatar-1.png',
-      selectedSpeciality: 'Челюстно-лицевой хирург',
-      selectedBigImg: 'images/specialists/avatar-7.png',
-      selectedExperience: 6,
-      selectedReview: 200,
-      selectedConsultation: 25,
-      buttons: [
-        { 
-          id: 1, 
-          name: 'Овсоян Григорий', 
-          category: 'Стоматолог-терапевт', 
-          img: 'images/specialists/avatar-4.png', 
-          imgBig:'images/specialists/avatar-4.png',
-          experience: 6,
-          review: 200,
-          consultation: 25,
-        },
-        { 
-          id: 2, 
-          name: 'Унаньян Карина', 
-          category: 'Челюстно-лицевой хирург', 
-          img: 'images/specialists/avatar-7.png', 
-          imgBig:'images/specialists/avatar-7.png',
-          experience: 6,
-          review: 200,
-          consultation: 25,
-        },
-        { 
-          id: 3, 
-          name: 'Маашев Магомед', 
-          category: 'Стоматолог-хирург', 
-          img: 'images/specialists/avatar-6.png', 
-          imgBig:'images/specialists/avatar-6.png',
-          experience: 6,
-          review: 200,
-          consultation: 25,
-        },
-        { 
-          id: 4, 
-          name: 'Шагалова Полина', 
-          category: 'Стоматолог', 
-          img: 'images/specialists/avatar-5.png', 
-          imgBig:'images/specialists/avatar-5.png',
-          experience: 6,
-          review: 200,
-          consultation: 25,
-        },
-      ],
-    };
-  },
-  methods: {
-    selectButton(buttonId) {
-      this.selectedButton = buttonId;
-      this.buttons.map(item => {
-        if (this.selectedButton === item.id) {
-          this.selectedName = item.name;
-          this.selectedImg = item.img;
-          this.selectedSpeciality = item.category;
-          this.selectedBigImg = item.imgBig;
-          this.selectedExperience = item.experience;
-          this.selectedReview = item.review;
-          this.selectedConsultation = item.consultation;
-        }
-      });
-    },
-  },
-  setup() {
-      const assetsStore = useAssets();
-    return {
-      assetsStore,
-    }
-  }
-};
-</script>
-
 <style lang="scss" scoped>
-@import '/assets/styles/style.scss';
+@import "/assets/styles/style.scss";
 .specialists {
   padding-top: 80px;
 }
 
 .div-with-line {
   border-radius: 0px 15px 16px 0px;
-  background: var(--accent, #232D5B);
+  background: var(--accent, #232d5b);
   width: 3px;
   height: 24px;
   position: absolute;
   left: -1px;
   top: 28px;
 }
+.specialists-subject {
+  display: flex;
+  align-items: baseline;
+  justify-content: space-between;
+  width: 529px;
+  max-width: 100%;
+  padding-bottom: 40px;
+}
+
+.specialists-heading {
+  @include body-22-medium-Neue;
+  color: $dark-blue-subtitle;
+}
+
+.specialists-heading-title {
+  @include body-22-medium-Neue;
+  color: $dark-blue-subtitle;
+}
+
+.specialists-desc {
+  @include body-16-regular;
+  color: $placeholder;
+}
+
+.specialists-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 36px;
+
+  .specialists-list {
+    display: flex;
+    flex-direction: column;
+    gap: 14px;
+
+    .active {
+      position: relative;
+    }
+  }
+
+  .specialists-btn {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    border-radius: 10px;
+    padding: 10px;
+    border: 1px solid var(--stroke, #e9e9e9);
+    background: white;
+
+    &:hover {
+      border: 1px solid #d5d5d5;
+      cursor: pointer;
+      transition: all 0.3s ease-in-out;
+      color: var(--accent, #232d5b);
+    }
+
+    .specialists-box__name {
+      &:hover {
+        color: $hover;
+      }
+    }
+
+    .specialists-box__name {
+      &:active {
+        color: $dark-blue-subtitle;
+      }
+    }
+
+    &:active {
+      border: 1px solid var(--stroke, #e9e9e9);
+      color: $click;
+    }
+
+    &__img {
+      width: 60px;
+      height: 60px;
+      border-radius: 5px;
+      background: #e8e8e8;
+    }
+  }
+}
+
+.specialists-box {
+  @include flex-column-start;
+  gap: 10px;
+
+  &__name {
+    @include body-18-regular;
+    color: $dark-blue-subtitle;
+  }
+
+  &__spec {
+    @include body-14-regular;
+    opacity: 0.7;
+    color: $gray-text;
+  }
+}
+
+.specialists-inner {
+  display: flex;
+  gap: 16px;
+
+  &__img {
+    width: 308px;
+    height: 308px;
+    img {
+      width: 308px;
+      height: 308px;
+      border-radius: 15px;
+    }
+  }
+
+  &__info {
+    @include flex-column-start;
+    // justify-content: space-between;
+    gap: 30px;
+
+    &-name {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+      padding-top: 10px;
+    }
+
+    &-numbers {
+      display: flex;
+      align-items: center;
+      text-align: center;
+      gap: 20px;
+
+      .specialists-numbers {
+        @include body-22-semi-bold;
+        color: $hover;
+      }
+    }
+
+    &-btn {
+      display: flex;
+      align-items: center;
+      gap: 20px;
+      margin-top: 77px;
+    }
+  }
+}
+
+@media (max-width: 1018px) {
+  .specialists {
+    padding-top: 0;
+  }
   .specialists-subject {
     display: flex;
-    align-items: baseline;
-    justify-content: space-between;
-    width: 529px;
-    max-width: 100%;
-    padding-bottom: 40px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+  }
+
+  .specialists-inner__info-btn {
+    margin-top: 0;
   }
 
   .specialists-heading {
-    @include body-22-medium-Neue;
-    color: $dark-blue-subtitle;
-  }
-
-  .specialists-heading-title {
-    @include body-22-medium-Neue;
-    color: $dark-blue-subtitle;
-  }
-
-  .specialists-desc {
-    @include body-16-regular;
-    color: $placeholder;
+    font-size: 20px;
   }
 
   .specialists-container {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 36px;
+    gap: 20px;
 
     .specialists-list {
-      display: flex;
-      flex-direction: column;
-      gap: 14px;
-
-        .active {
-          position: relative;
-        }
-    }
-
-      .specialists-btn {
-        display: flex;
-        align-items: center;
-        gap: 10px;
-        border-radius: 10px;
-        padding: 10px;
-        border: 1px solid var(--stroke, #E9E9E9);
-        background: white;
-
-        &:hover {
-          border: 1px solid #D5D5D5;
-          cursor: pointer;
-          transition: all 0.3s ease-in-out;
-          color: var(--accent, #232D5B);
-      }
-
-      .specialists-box__name {
-        &:hover {
-          color: $hover;
-        }
-      }
-
-      .specialists-box__name {
-        &:active  {
-          color: $dark-blue-subtitle;
-        }
-      }
-
-        &:active {
-          border: 1px solid var(--stroke, #E9E9E9);
-          color: $click;
-        }
-
-          &__img {
-            width: 60px;
-            height: 60px;
-            border-radius: 5px;
-            background: #E8E8E8;
-          }
-      } 
-  }
-
-
-  .specialists-box {
-    @include flex-column-start;
-    gap: 10px;
-
-      &__name {
-        @include body-18-regular;
-        color: $dark-blue-subtitle;
-      }
-
-      &__spec {
-        @include body-14-regular;
-        opacity: 0.7;
-        color: $gray-text;
-      }
-  }
-
-  .specialists-inner {
-    display: flex;
-    gap: 16px;
-
-      &__img {
-        width: 308px;
-        height: 308px;
-          img {
-            width: 308px;
-            height: 308px;
-            border-radius: 15px;
-          }
-      }
-
-      &__info {
-        @include flex-column-start;
-        // justify-content: space-between;
-        gap: 30px;
-
-        &-name {
-          display: flex;
-          flex-direction: column;
-          gap: 10px;
-          padding-top: 10px;
-        }
-
-        &-numbers{
-          display: flex;
-          align-items: center;
-          text-align: center;
-          gap: 20px;
-
-          .specialists-numbers {
-            @include body-22-semi-bold;
-            color: $hover;
-          }
-        }
-
-        &-btn {
-          display: flex;
-          align-items: center;
-          gap: 20px;
-          margin-top: 77px;
-        }
-      }
-  }
-
-  @media (max-width: 1018px) {
-    .specialists {
-      padding-top: 0;
-    }
-    .specialists-subject {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
+      flex-direction: unset;
+      overflow-x: scroll;
+      white-space: nowrap;
       gap: 10px;
     }
-
-    .specialists-inner__info-btn {
-      margin-top: 0;
+    .specialists-list::-webkit-scrollbar {
+      display: none;
     }
-
-    .specialists-heading {
-      font-size: 20px;  
-    }
-
-    .specialists-container {
-      gap: 20px;
-
-      .specialists-list {
-        flex-direction: unset;
-        overflow-x: scroll;
-        white-space: nowrap;
-        gap: 10px;
-      }
-      .specialists-list::-webkit-scrollbar {
-        display: none;
-      }
-    }
+  }
 
   .specialists-inner {
     gap: 14px;
-      &__img {
-        width: 332px;;
+    &__img {
+      width: 332px;
+      height: 333px;
+
+      img {
+        width: 333px;
         height: 333px;
-
-          img {
-            width: 333px;
-            height: 333px;
-          }
       }
+    }
 
-      &__info {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        justify-content: center;
-        gap: 20px;
-      }
+    &__info {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: center;
+      gap: 20px;
+    }
   }
 }
 
-  @media (max-width: 734px) {
-    .specialists-inner {
-      width: 100%;
-      gap: 14px;
+@media (max-width: 734px) {
+  .specialists-inner {
+    width: 100%;
+    gap: 14px;
 
-       &__img {
-        width: 38%;
-        height: 333px;
-        border-radius: 15px;
-        border-left: 1px solid  #EDEDED;
-        border-right: 1px solid  #EDEDED;
+    &__img {
+      width: 38%;
+      height: 333px;
+      border-radius: 15px;
+      border-left: 1px solid #ededed;
+      border-right: 1px solid #ededed;
 
-          img {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            object-position: 37% 74%;
-          }
-        }
-    }
-}
-
-  @media (max-width: 734px) {
-    .specialists {
-      margin: 0 auto 80px !important;
-    }
-    .specialists-btn-base {
-      width: 80%;
-      height: auto;
-    }
-    .specialists-inner__info-btn {
-      flex-wrap: wrap;
-      padding-bottom: 0;
-    }
-    .specialists-inner {
-      width: 100%;
-      gap: 10px;
-
-       &__img {
-        width: 50%;
-       }
-    }
-}
-
-  @media (max-width: 585px) {
-    .specialists-btn-base {
-      width: 100%;
-    }
-
-    .specialists-inner {
-       &__img {
-        width: 67%;
-
-       }
-    }
-}
-  @media (max-width: 556px) {
-    .specialists-inner {
-      flex-wrap: wrap;
-
-      &__img {
-        width: 343px;
-        height: 343px;
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: 37% 74%;
       }
+    }
+  }
+}
 
-      &__info-btn {
-        flex-wrap: nowrap;
-      }
+@media (max-width: 734px) {
+  .specialists {
+    margin: 0 auto 80px !important;
+  }
+  .specialists-btn-base {
+    width: 80%;
+    height: auto;
+  }
+  .specialists-inner__info-btn {
+    flex-wrap: wrap;
+    padding-bottom: 0;
+  }
+  .specialists-inner {
+    width: 100%;
+    gap: 10px;
+
+    &__img {
+      width: 50%;
+    }
+  }
+}
+
+@media (max-width: 585px) {
+  .specialists-btn-base {
+    width: 100%;
+  }
+
+  .specialists-inner {
+    &__img {
+      width: 67%;
+    }
+  }
+}
+@media (max-width: 556px) {
+  .specialists-inner {
+    flex-wrap: wrap;
+
+    &__img {
+      width: 343px;
+      height: 343px;
+    }
+
+    &__info-btn {
+      flex-wrap: nowrap;
+    }
   }
   .specialists-btn-base {
     width: max-content;
--- pages/index.vue
+++ pages/index.vue
@@ -1,61 +1,104 @@
+<script setup>
+const apiBaseUrl = useRuntimeConfig().public.apiBaseUrl;
+const baseUrl = useRuntimeConfig().public.baseUrl;
+const assetsStore = useAssets();
+
+const { data: fourSpecialists } = await useFetch(`${apiBaseUrl}specialists`, {
+  query: {
+    "pagination[page]": 1,
+    "pagination[pageSize]": 4,
+    populate: "deep",
+  },
+});
+
+const mainSpecialists = fourSpecialists.value?.data?.map((sp) => {
+  return {
+    id: sp?.id,
+    name: `${sp?.attributes?.firstName} ${sp?.attributes?.lastName}`,
+    category: sp?.attributes?.position,
+    img: sp?.attributes?.fotoSpecialist?.data?.attributes?.formats?.thumbnail
+      ?.url
+      ? baseUrl +
+        sp?.attributes?.fotoSpecialist?.data?.attributes?.formats?.thumbnail
+          ?.url
+      : assetsStore.useAsset("images/icons/logo.svg"),
+    imgBig: sp?.attributes?.fotoSpecialist?.data?.attributes?.url
+      ? baseUrl + sp?.attributes?.fotoSpecialist?.data?.attributes?.url
+      : assetsStore.useAsset("images/icons/logo.svg"),
+    experience: {
+      year: sp?.attributes?.meetingPerson?.dataMeeting?.[0]?.total,
+      text: sp?.attributes?.meetingPerson?.dataMeeting?.[0]?.item,
+    },
+    review: {
+      year: sp?.attributes?.meetingPerson?.dataMeeting?.[1]?.total,
+      text: sp?.attributes?.meetingPerson?.dataMeeting?.[1]?.item,
+    },
+    consultation: {
+      year: sp?.attributes?.meetingPerson?.dataMeeting?.[2]?.total,
+      text: sp?.attributes?.meetingPerson?.dataMeeting?.[2]?.item,
+    },
+  };
+});
+
+console.log(mainSpecialists);
+</script>
+
 <template>
   <blocks-main-primary-banner />
-    <BlocksMainBanner 
-      :title="'Лечим не только зубы'"
-      :text="'Многопрофильная детская клиника СтамусМед'"
-      :titleLink="'Узнать подробнее'"
-      link="/detskaya-klinika"
-      bgColor="light-blue-gradient"
-      type="true"
-      img="girl.png"
-    />
-    <blocks-main-areas />
-    <blocks-main-specialists />
-    <BlocksMainBanner 
-      :title="'Добровольное медицинское страхование'"
-      :text="'которое в отличие от обязательного обеспечивает получение гражданином дополнительных медицинских услуг'"
-      :titleLink="'Узнать подробнее'"
-      link="#"
-      bgColor="light-blue"
-      type="true"
-      img="cards2.png"
-      bigImg=true
-      :handleLinkClick="openBidModal"
-    />
-    <blocks-main-popularServices />
-    <BlocksMainBanner 
-      :selectedText="true"
-      :text="'Независимый всеройсийский портал Продокторов'"
-      :titleLink="'Записаться онлайн'"
-      link="#"
-      bgColor="blue-gradient"
-      img="mobile.svg"
-      bigImg=true
-    />
-    <blocks-main-feedback />
-    <BlocksMainBanner 
-      :title="'Лучшая детская стоматология'"
-      :text="'среди частных детских стоматологий Краснодара по версии экспертного журнала Startsmile при поддержке ИД “КоммерстантЪ”'"
-      :additionalText="' *1 место в рейтинге детских стоматологических клиник Краснодара за 2022 (клиники, которым более 3-х лет)'"
-      :titleLink="'Узнать подробнее'"
-      link="/detskaya-stomatologiya"
-      bgColor="dark-red"
-      img="image.png"
-    />
-    <blocks-main-articles title="Статьи"/>
-    <blocks-main-seo 
-      title="СЕО текст"
-      text="Как уже неоднократно упомянуто, непосредственные участники технического прогресса неоднозначны и будут разоблачены.
-            С учётом сложившейся международной обстановки, высококачественный прототип будущего проекта не даёт нам иного выбора, 
-            кроме определения приоретизации разума над эмоциями. Разнообразный и богатый опыт говорит нам, 
+  <BlocksMainBanner
+    :title="'Лечим не только зубы'"
+    :text="'Многопрофильная детская клиника СтамусМед'"
+    :titleLink="'Узнать подробнее'"
+    link="/detskaya-klinika"
+    bgColor="light-blue-gradient"
+    type="true"
+    img="girl.png"
+  />
+  <blocks-main-areas />
+  <blocks-main-specialists :mainSpecialists="mainSpecialists" />
+  <BlocksMainBanner
+    :title="'Добровольное медицинское страхование'"
+    :text="'которое в отличие от обязательного обеспечивает получение гражданином дополнительных медицинских услуг'"
+    :titleLink="'Узнать подробнее'"
+    link="#"
+    bgColor="light-blue"
+    type="true"
+    img="cards2.png"
+    bigImg="true"
+    :handleLinkClick="openBidModal"
+  />
+  <blocks-main-popularServices />
+  <BlocksMainBanner
+    :selectedText="true"
+    :text="'Независимый всеройсийский портал Продокторов'"
+    :titleLink="'Записаться онлайн'"
+    link="#"
+    bgColor="blue-gradient"
+    img="mobile.svg"
+    bigImg="true"
+  />
+  <blocks-main-feedback />
+  <BlocksMainBanner
+    :title="'Лучшая детская стоматология'"
+    :text="'среди частных детских стоматологий Краснодара по версии экспертного журнала Startsmile при поддержке ИД “КоммерстантЪ”'"
+    :additionalText="' *1 место в рейтинге детских стоматологических клиник Краснодара за 2022 (клиники, которым более 3-х лет)'"
+    :titleLink="'Узнать подробнее'"
+    link="/detskaya-stomatologiya"
+    bgColor="dark-red"
+    img="image.png"
+  />
+  <blocks-main-articles title="Статьи" />
+  <blocks-main-seo
+    title="СЕО текст"
+    text="Как уже неоднократно упомянуто, непосредственные участники технического прогресса неоднозначны и будут разоблачены.
+            С учётом сложившейся международной обстановки, высококачественный прототип будущего проекта не даёт нам иного выбора,
+            кроме определения приоретизации разума над эмоциями. Разнообразный и богатый опыт говорит нам,
             что укрепление и развитие внутренней структуры в значительной степени обусловливает важность системы массового участия."
-      img="rectangle.png"      
-    />
-    <blocks-main-form />
+    img="rectangle.png"
+  />
+  <blocks-main-form />
 </template>
 
-
 <style lang="scss" scoped>
-@import '../assets/styles/style.scss';
-
+@import "../assets/styles/style.scss";
 </style>
