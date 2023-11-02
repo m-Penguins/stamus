b56456322fa8e74917e5fb5fc8f5482c7ec004d3
MOONcitizenX <mooncitizenx@gmail.com>
2023-11-02 18:28:54 +0300

    feat: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,43 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   // ... other options
+  app: {
+    head: {
+      charset: "utf-8",
+      htmlAttrs: {
+        lang: "ru"
+      },
+      viewport:
+	"width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no",
+      link: [
+	{ rel: "icon", type: "image/png", href: "/favicon/favicon.ico" },
+	{
+	  rel: "apple-touch-icon",
+	  sizes: "180x180",
+	  href: "/favicon/apple-touch-icon.png",
+	},
+	{
+	  rel: "icon",
+	  type: "image/png",
+	  sizes: "32x32",
+	  href: "/favicon/favicon-32x32.png",
+	},
+	{
+	  rel: "icon",
+	  type: "image/png",
+	  sizes: "16x16",
+	  href: "/favicon/favicon-16x16.png",
+	},
+	{ rel: "manifest", href: "/favicon/site.webmanifest" },
+	{ rel: "mask-icon", href: "/favicon/safari-pinned-tab.svg", color: "#5bbad5" },
+      ],
+      meta: [
+	{ name: "msapplication-TileColor", content: "#2b5797" },
+	{ name: "theme-color", content: "#ffffff" },
+	{ name: "msapplication-config", content: "/favicon/browserconfig.xml" },
+      ],
+    },
+  },
   modules: [
     "@pinia/nuxt",
     [
