57a0e5b12d865bb08be48670bb866d41f18ec829
Dastan <daskov2412@gmail.com>
2024-08-28 15:09:05 +0600

    changed banner styles

--- components/dynamicBlock/Banner.vue
+++ components/dynamicBlock/Banner.vue
@@ -131,9 +131,8 @@ const props = defineProps(["block"]);
 
 .banner-container {
   display: grid;
-
-  // height: 100%;
-  height: 480px;
+  height: fit-content;
+  max-height: 480px;
 
   grid-template-columns: [text-start bg-start] 1fr [text-end image-start] 1fr [image-end bg-end];
 
