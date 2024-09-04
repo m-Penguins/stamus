066a7e7c620b4ec61e409a4c0a5e8d276470de4e
Dastan <daskov2412@gmail.com>
2024-09-04 17:03:34 +0600

    fixed description in page about company

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -3,9 +3,9 @@
     <div class="about-page-wrap">
       <elements-bread-crumbs :breadcrumbs="breadcrumbs" />
       <h1 class="about-page-title">О компании</h1>
-      <div class="about-page-description">
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-      </div>
+<!--      <div class="about-page-description">-->
+<!--        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.-->
+<!--      </div>-->
       <BlocksMapper
         :blocks="blocks"
       />
