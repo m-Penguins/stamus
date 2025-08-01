2175909b92892e0a00d303d3e78f9adce611d1b1
suleimanDev <suliman16042005@gmail.com>
2025-08-01 20:59:24 +0600

    1115

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -11,13 +11,8 @@ const tagFilter = ref(
   Array.isArray(route.query.tag)
     ? route.query.tag
     : route.query.tag
-<<<<<<< HEAD
     ? [route.query.tag]
     : [],
-=======
-      ? [route.query.tag]
-      : [],
->>>>>>> 36491a0 (#1115)
 );
 const totalItems = ref(0);
 
